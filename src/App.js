bd095f9320986310fbe94582d574d977f1546f96
Kassandra Meyer <kassandra@whereby.us>
2017-07-02 19:43:41 -0400

    add routing

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom'
 import * as BooksAPI from './BooksAPI'
 import ListBooks from './ListBooks'
 import SearchBook from './SearchBook'
@@ -16,8 +17,12 @@ class BooksApp extends Component {
   render() {
     return (
       <div className="app">
+     <Route exact path='/' render={() => (
         <ListBooks books={this.state.books} />
+      )}/>
+      <Route path='/search' render={() => (
         <SearchBook books={this.state.books} />
+      )}/>
       </div>
     );
   }
--- src/Bookshelf.js
+++ src/Bookshelf.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-// import { Link } from 'react-router-dom'
 import PropTypes from 'prop-types'
 // import escapeRegExp from 'escape-string-regexp'
 // import sortBy from 'sort-by'
