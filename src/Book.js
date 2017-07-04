9f7376ce56ee80872ee5831ab35ca7fe63a6f3b9
Kassandra Meyer <kassandra@whereby.us>
2017-07-04 18:53:06 -0400

    map array of authors into separate div tags

--- src/Book.js
+++ src/Book.js
@@ -26,7 +26,11 @@ class Book extends Component {
 		        </div>
 		      </div>
 		      <div className="book-title">{book.title}</div>
-		      <div className="book-authors">{book.authors}</div>
+		      <div className="book-authors">
+				{book.authors.map(author => (
+					<div key={author}>{author}</div>
+				))}
+		      </div>
 		    </div>
 	    </li>
     )
