29bc9c2a88ba01bf12ae0299813e1cd4e11682bd
Kassandra Meyer <kassandra@whereby.us>
2017-07-08 10:22:16 -0400

    type styling

--- src/components/Book.js
+++ src/components/Book.js
@@ -35,7 +35,7 @@ class Book extends Component {
 		        </div>
 		      </div>
 		      <div className="book-title">{book.title}</div>
-		      <div className="book-authors">{book.authors}</div>		      
+		      <div className="book-authors">{book.authors[0]}</div>		      
 		       {/*<div className="book-authors">
 				{book.authors.map(author => (
 					<div key={author}>{author}</div>
